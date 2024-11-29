574d758181dbad27b92bcbc9d4378a4c41d83769
Jacob Scherber <jgscherber@gmail.com>
2024-11-29 14:46:03 -0500

    base

--- PluginDevVault/.obsidian/plugins/obsidian-sample-plugin/main.ts
+++ PluginDevVault/.obsidian/plugins/obsidian-sample-plugin/main.ts
@@ -10,11 +10,15 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
+export default class HelloWorldPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+
+        this.addRibbonIcon('dice', 'Greet', () => {
+            new Notice('Hello, World!');            
+        });
 
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
@@ -108,9 +112,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: HelloWorldPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HelloWorldPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
